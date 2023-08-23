d59a0421532cd4ea3bf9dbe7986e8978df8a6bc3
yu-furukubo <138659773+yu-furukubo@users.noreply.github.com>
2023-08-23 05:39:01 +0000

    [restart]adv-kadai-6

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,53 @@
+class UsersController < ApplicationController
+  before_action :ensure_correct_user, only: [:update, :edit]
+
+  def show
+    @user = User.find(params[:id])
+    @books = @user.books
+    @book = Book.new
+  end
+
+  def index
+    @users = User.all
+    @book = Book.new
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    if @user.update(user_params)
+      redirect_to user_path(@user), notice: "You have updated user successfully."
+    else
+      render "edit"
+    end
+  end
+
+  def follow_users
+    @user = User.find(params[:id])
+    @users = @user.following_users
+  end
+
+  def follower_users
+    @user = User.find(params[:id])
+    @users = @user.followed_users
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :introduction, :profile_image)
+  end
+
+  def book_params
+    params.require(:book).permit(:title, :body, :user_id)
+  end
+
+  def ensure_correct_user
+    @user = User.find(params[:id])
+    unless @user == current_user
+      redirect_to user_path(current_user)
+    end
+  end
+end
