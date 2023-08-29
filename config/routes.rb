6816c71fd5ffe07eaa754b3dab25392a12bb9682
yu-furukubo <138659773+yu-furukubo@users.noreply.github.com>
2023-08-29 08:45:10 +0000

    [finish]guestlogin

--- config/routes.rb
+++ config/routes.rb
@@ -16,5 +16,9 @@ Rails.application.routes.draw do
     resource :relationship, only: [:create, :destroy]
   end
 
+  devise_scope :user do
+    post "users/guest_sign_in", to: "users/sessions#guest_sign_in"
+  end
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
